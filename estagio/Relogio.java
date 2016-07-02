a97c958261f500c3964f81308815b68ff2d6d7d4
Gabriel Rohden <gabriel_rohden@outlook.com>
2016-07-02 15:31:45 -0300

    Update comment
    Some problem with angle calculation, seems to be wrong from the given
    example.

--- estagio/Relogio.java
+++ estagio/Relogio.java
@@ -10,9 +10,21 @@ public class Relogio {
 	/* Constantes para ajuda nos calculos 
 	 * Usei byte pois aloca 8 bits, e isso ja é o bastante visto que
 	 * o numero maximo de 8 bits é 255.
+	 *
+	 * 360 graus divididos em 12 horas = 30 graus/hora 
 	 */
-	private static final byte anguloPorHora=30; /*360 graus divididos em 12 horas = 30 graus/hora*/
-	private static final byte anguloPorMinuto=3; /*15 graus a cada 5 minutos, 3 graus a cada 1 minuto.*/
+	private static final byte anguloPorHora=30; 
+	/*
+	 * Aparentemente a descriçao da prova esta errada
+	 * Quando o horario for 00:15 o angulo dos ponteiros sera 90 graus
+	 * com 00:59 minutos teriamos um angulo de 177 graus no caso acima. (nao considerando movimento do pnteiro das horas em relaçao aos minutos)
+	 * Caso seja assim, entao o "circulo" teria so 180 graus, e o calculo
+	 * do anguloPorHora seria  180/12.
+	 * O calculo da variavel abaixo deveria ser feito como 360 graus / 60 minutos
+	 * Mas como nao sei se foi proposital, vou deixar o calculo como:
+	 * 15 graus a cada 5 minutos, 3 graus a cada 1 minuto.
+	 */
+	private static final byte anguloPorMinuto=3; 
 
 	
 	/**
